2ecb541b886e4929ed99c805ea96c73e94438b77
aakash kumar tripathi <92784014+Aakashfit@users.noreply.github.com>
2022-02-17 11:20:11 +0530

    swiggy

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,16 +1,35 @@
-const router=require("express").Router();
+const  app  = require("express");
+
+//const router=require("express").Router();
 const stripe=require("stripe")(process.env.STRIPE_KEY)
-router.post("/payment",(req,res)=>{
-    stripe.charge.create({
-        source:req.body.tokenid,
-        amount:req.body.amount,
-        currency:"inr",
-    },(stripeError,stripeRes)=>{
-        if(stripeError){
-            res.json(stripeError)
-        }else{
-            res.json(stripeRes);
-        }
-    })
-})
+// const publishable_key="pk_test_51KQtZ1SEQ1Q1nawWLutPTYLP2flhvJbC0TCgYwAXcDJHUZOib5roJZDkZKQDrURbGOA6owe7w6stOeZycx75AX3I00X72eNMu1"
+// const secret_key="sk_test_51KQtZ1SEQ1Q1nawWR6ueERYermSxYNfj5BHyGOflugfUOI2YwZl1jjrrpYGCI83DQMqT3a32LHFSzEoheGAZbZnl00FCwjsjYY"
+// const path=require("path")
+
+// app.get("/payment",(req,res)=>{
+//     res.render(`home`,{
+//         key:publishable_key
+//     })
+// })
+
+
+
+
+
+
+
+
+// router.post("/payment",(req,res)=>{
+//     stripe.charge.create({
+//         source:req.body.tokenid,
+//         amount:req.body.amount,
+//         currency:"inr",
+//     },(stripeError,stripeRes)=>{
+//         if(stripeError){
+//             res.json(stripeError)
+//         }else{
+//             res.json(stripeRes);
+//         }
+//     })
+// })
 module.exports=router;
--- server/server.js
+++ server/server.js
@@ -5,13 +5,14 @@ require('./models/db/connectDB');
 
 const restaurantRouter = require('./routes/restaurant')
 const customerRouter = require('./routes/customer')
-const deliveryPartnerRouter = require('./routes/deliveryPartner')
+const deliveryPartnerRouter = require('./routes/deliveryPartner');
+const bodyParser = require('body-parser');
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
 app.use(express.static(path.resolve(__dirname, '../client/build')))
-
+app.set("view engine","ejs");
 app.use(express.json())
 app.use(cors())
 
@@ -22,6 +23,55 @@ app.use("/api/deliverypartners", deliveryPartnerRouter)
 app.get("/api", (req, res) => {
   res.json({message: 'This is swiggy backend'})
 });
+
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+//const bodyParser=require("body-parser")
+const publishable_key="pk_test_51KQtZ1SEQ1Q1nawWLutPTYLP2flhvJbC0TCgYwAXcDJHUZOib5roJZDkZKQDrURbGOA6owe7w6stOeZycx75AX3I00X72eNMu1";
+const secret_key="sk_test_51KQtZ1SEQ1Q1nawWR6ueERYermSxYNfj5BHyGOflugfUOI2YwZl1jjrrpYGCI83DQMqT3a32LHFSzEoheGAZbZnl00FCwjsjYY"
+//const path=require("path")
+
+const stripe = require('stripe')("sk_test_51KQtZ1SEQ1Q1nawWR6ueERYermSxYNfj5BHyGOflugfUOI2YwZl1jjrrpYGCI83DQMqT3a32LHFSzEoheGAZbZnl00FCwjsjYY") 
+app.set('views', path.join(__dirname, 'views')) 
+
+app.get("/payment",(req,res)=>{
+    res.render('home',{
+        key:publishable_key
+    })
+})
+app.post('/payment', function(req, res){ 
+ 
+  stripe.customers.create({ 
+      email: req.body.stripeEmail, 
+      source: req.body.stripeToken, 
+      name: 'SWIGGY', 
+      address: { 
+          line1: 'NEW SAHARA ESTATE', 
+          postal_code: '110092', 
+          city: 'New Delhi', 
+          state: 'Delhi', 
+          country: 'India', 
+      } 
+  }) 
+  .then((customer) => { 
+
+      return stripe.charges.create({ 
+          amount: 7000,     
+          description: 'BILL AMOUNT', 
+          currency: 'USD', 
+          customer: customer.id 
+      }); 
+  }) 
+  .then((charge) => { 
+      res.send("Success") 
+  }) 
+  .catch((err) => { 
+      res.send(err)     
+  }); 
+}) 
 
 
 
